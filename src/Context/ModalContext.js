81a3e2c57234f34bdefeb03f3238b6e72fd2b4d4
Druiz73 <davidruizeas@gmail.com>
2020-08-04 11:30:37 -0300

    resolving conflicts

--- src/Context/ModalContext.js
+++ src/Context/ModalContext.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext, createContext } from 'react';
+import React, { useState, useEffect,  createContext } from 'react';
 import axios from 'axios'
 
 export const ModalContext = createContext();
